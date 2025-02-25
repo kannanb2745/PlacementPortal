89178c57360a29d7ce87065c4d170294a00dc077
kannanb2745 <kannan.b2745@gmail.com>
2025-02-25 21:12:17 +0530

    feat

--- config/middleware.js
+++
@@ -1,8 +0,0 @@
-// setting flash notification middleware
-module.exports.setFlash = async(req, res, next)=>{
-    res.locals.flash = {
-        'success': req.flash('success'),
-        'error':req.flash('error')
-    }
-    next();
-}
--- routes/jobs.js
+++
@@ -1,9 +0,0 @@
-//import requried packeges
-const express = require("express");
-const router = express.Router();
-
-const jobController = require("../controllers/job_Controller");
-
-router.get('/fetch-jobs', jobController.fetch);
-
-module.exports = router;
